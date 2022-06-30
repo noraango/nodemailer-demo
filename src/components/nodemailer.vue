b841f468d72c8e2904e7519d2be0de6b3333e7d8
Zyta <timer217@gmail.com>
2022-06-30 02:44:49 +0200

    delete helloworld.vue

---
+++ src/components/nodemailer.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>hello</div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from "vue-property-decorator";
+
+@Component
+export default class Nodemailer extends Vue {}
+</script>
+
+<style scoped lang="scss"></style>
